82641341b5b04d852b77db2c06052dc915f09758
Jose Eduardo Sierra Vargas <jsierrav14@gmail.com>
2020-08-17 13:59:52 -0500

    input added

--- screens/NewPlaceScreen.js
+++ screens/NewPlaceScreen.js
@@ -1,18 +1,51 @@
-import React from 'react'
-import { StyleSheet, View, Text } from 'react-native'
+import React,{useState} from 'react'
+import { StyleSheet, View, Text, TextInput, ScrollView, Button } from 'react-native'
+import Colors from '../constants/Colors'
 
-const  NewPlaceScreen =  props =>{
-   return (
-       <View>
-    <Text>PlacesDetailScreen</Text>
-    </View>)
+const NewPlaceScreen = props => {
+
+    const [titleValue, setTitleValue] = useState('')
+    const titleChangeHandler = text =>{
+        setTitleValue(text)
+    }
+
+    const savePlaceHandler = ()=>{
+        
+    }
+    return (
+        <ScrollView>
+          <View style={styles.form}>
+          <Text style={styles.label}>Title</Text>
+          <TextInput 
+          style={styles.textInput} 
+          onChangeText={titleChangeHandler}
+          value={titleValue}/>
+          <Button title='Save place' color={Colors.primary} onPress={()=>{}}/>
+          </View>
+        
+        </ScrollView>
+        )
 }
 
-NewPlaceScreen.navigationOptions ={
-    headerTitle:'New Place'
+NewPlaceScreen.navigationOptions = {
+    headerTitle: 'New Place'
 }
 
 const styles = StyleSheet.create({
+    form:{
+        margin:30
+    },
+    label:{
+        fontSize:17,
+        marginBottom:15
+    },
+    textInput:{
+        borderBottomColor:'#ccc',
+        borderBottomWidth:1,
+        marginBottom:15,
+        paddingVertical:4,
+        paddingHorizontal:2
+    }
 
 })
 
